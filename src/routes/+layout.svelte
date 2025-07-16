825ba19de3760c5d95152f01655a1824e50c5ac0
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-07-16 12:39:17 +0300

    style: add site header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-
+	import SiteHeader  from '../lib/components/site-header/+page.svelte';
 	let { children } = $props();
 </script>
 
+<SiteHeader />
 {@render children()}
